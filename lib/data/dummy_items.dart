7f08b9cc1bada9e1702482e9589266d4f4e4e923
Ahmed Hany Mohamed <Ahmed.hanymohamed@student.giu-uni.de>
2025-08-30 12:47:11 +0300

    altering logic

--- lib/data/dummy_items.dart
+++ lib/data/dummy_items.dart
@@ -1,31 +1,45 @@
+import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 
-class DummyItems {
-  static final List<Map<String, dynamic>> items = [
-    {
-      'name': 'Apple',
-      'category': 'Fruits',
-      'color': Categories.data['Fruits'],
-    },
-    {
-      'name': 'Carrot',
-      'category': 'Vegetables',
-      'color': Categories.data['Vegetables'],
-    },
-    {
-      'name': 'Milk',
-      'category': 'Dairy',
-      'color': Categories.data['Dairy'],
-    },
-    {
-      'name': 'Bread',
-      'category': 'Bakery',
-      'color': Categories.data['Bakery'],
-    },
-    {
-      'name': 'Chicken',
-      'category': 'Meat',
-      'color': Categories.data['Meat'],
-    }
-    ];
+class GroceryItem {
+  const GroceryItem({
+    required this.name,
+    required this.category,
+    required this.color,
+  });
+
+  final String name;
+  final String category;
+  final Color color;
+}
+
+
+class GroceryItems {
+  static final List<GroceryItem> items = [
+    GroceryItem(
+      name: 'Apple',
+      category: 'Fruits',
+      color: Categories.data['Fruits']!,
+    ),
+    GroceryItem(
+      name: 'Carrot',
+      category: 'Vegetables',
+      color: Categories.data['Vegetables']!,
+    ),
+    GroceryItem(
+      name: 'Milk',
+      category: 'Dairy',
+      color: Categories.data['Dairy']!,
+    ),
+    GroceryItem(
+      name: 'Bread',
+      category: 'Bakery',
+      color: Categories.data['Bakery']!,
+    ),
+    GroceryItem(
+      name: 'Chicken',
+      category: 'Meat',
+      color: Categories.data['Meat']!,
+    ),
+  ];
 }
